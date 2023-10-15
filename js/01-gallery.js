ed756ef1c7604ef1478d7f88bd31cecf637e4f36
nonamebbx <melnicukartem321@gmail.com>
2023-10-15 22:42:20 +0200

    new

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,24 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні
-// Для створення галереї використовуй масив об'єктів зображень.
-// Для створення розмітки елемента галереї використовуй шаблонну строку і insertAdjacentHTML().
 
-const galleryList = document.querySelector('.gallery');
+const list = document.querySelector('.gallery');
 
-galleryList.insertAdjacentHTML("beforeend", createMarkupList(galleryItems));
-galleryList.addEventListener('click', handleClick);
+list.insertAdjacentHTML("beforeend", createMarkupList(galleryItems));
+list.addEventListener('click', handleClick);
 
-function createMarkupList(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `
-        <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-        </li>
-        `;
-    }).join('');
-}
+function createMarkupList(arr) {
+    return arr.map(({ preview, description, original }) => `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+     />
+     </a>
+    </li>
+    `).join(' ');
+};
 
 function handleClick(event) {
     event.preventDefault();
@@ -51,5 +50,3 @@ function handleClick(event) {
         instance.show();
     }
 };
-
-console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const list = document.querySelector('.gallery');
+
+list.insertAdjacentHTML('beforeend', createMarkupList(galleryItems));
+
+
+function createMarkupList(arr) {
+  return arr.map(({ preview, description, original }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `)
+    .join(' ');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
